f5c46a06608d0e95caecff847bd77f4371015b7f
COMPUS021\daw2 <adrianhoyos098@gmail.com>
2025-11-07 18:22:05 +0100

    no message

--- listamorosos/src/main/java/com/example/listamorosos/controller/MorosoController.java
+++ listamorosos/src/main/java/com/example/listamorosos/controller/MorosoController.java
@@ -1,31 +1,40 @@
 package com.example.listamorosos.controller;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.listamorosos.model.Moroso;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping
 public class MorosoController {
 
     public LinkedList <Moroso> lista = new LinkedList<>();
 
-    @GetMapping("/Principal")
+    @GetMapping("/api/morosos/principal")
     public LinkedList<Moroso> obtenerMorosos(){
         return lista;
     }
 
-    @PostMapping("/id")
-    public String agregarMoroso(@RequestBody Moroso newMoroso) {
-        lista.add(newMoroso);
-        return "Moroso agregado correctamente";
+    @PostMapping("/api/morosos/add")
+    public String agregarMorosos(@RequestBody List<Moroso> nuevosMorosos) {
+        lista.addAll(nuevosMorosos);
+        return "Morosos agregados correctamente";
+    }
+    
+    @DeleteMapping("/api/morosos/eliminar/{id}")
+    public void eliminarMoroso(@PathVariable int id) {
+        lista.remove(id);
     }
     
 
